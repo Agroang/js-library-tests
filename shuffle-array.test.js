17f37c31a1356970aa9d8bf0b7131622ca7981b0
matias acuna <agroang@gmail.com>
2022-01-14 16:29:30 +0900

    first test on Jest completed, very simple but still it's a start for testing

--- shuffle-array.test.js
+++ shuffle-array.test.js
@@ -1,5 +1,7 @@
 const shuffle = require('./shuffle-array')
 
+// run with yarn test
+
 test('array of 8 elements to have a length of 8', () => {
   expect(shuffle([1, 2, 3, 4, 5, 6, 7, 8])).toHaveLength(8);
 });
