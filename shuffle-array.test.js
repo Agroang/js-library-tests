2b5d756d368ee0275cd7319980d917d8f9f8e63b
matias acuna <agroang@gmail.com>
2022-01-14 16:41:48 +0900

    added another test to shuffle array function

--- shuffle-array.test.js
+++ shuffle-array.test.js
@@ -5,3 +5,17 @@ const shuffle = require('./shuffle-array')
 test('array of 8 elements to have a length of 8', () => {
   expect(shuffle([1, 2, 3, 4, 5, 6, 7, 8])).toHaveLength(8);
 });
+
+test('array to be in a different order', () => {
+  expect(shuffle([1, 2, 3, 4, 5, 6, 7, 8])).not.toBe([1, 2, 3, 4, 5, 6, 7, 8]);
+});
+
+describe('not.arrayContaining', () => {
+  const expected = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  it('matches if the actual array does not contain the expected elements', () => {
+    expect([1, 3, 10]).toEqual(
+      expect.not.arrayContaining(expected),
+    );
+  });
+});
