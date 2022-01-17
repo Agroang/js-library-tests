48693dcbb2c123588fb7037a51cd111ab05ca3a3
matias acuna <agroang@gmail.com>
2022-01-17 18:09:37 +0900

    added another test for celsius to fahrenheit function and fixed a few of the expected values messages

--- celsius-to-fahrenheit.test.js
+++ celsius-to-fahrenheit.test.js
@@ -4,10 +4,14 @@ test('returns a fahrenheit value of 59 from 15 that is in celsius', () => {
   expect(celsiusToFahrenheit(15)).toBe(59);
 });
 
-test('returns a fahrenheit value of 59 from 15 that is in celsius', () => {
+test('returns a fahrenheit value of 32 from 0 that is in celsius', () => {
   expect(celsiusToFahrenheit(0)).toBe(32);
 });
 
-test('returns a fahrenheit value of 59 from 15 that is in celsius', () => {
+test('returns a fahrenheit value of -20 from -4 that is in celsius', () => {
   expect(celsiusToFahrenheit(-20)).toBe(-4);
+});
+
+test('returned value to be truthy', () => {
+  expect(celsiusToFahrenheit(1)).toBeTruthy();
 });
