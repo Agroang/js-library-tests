c1adcee7a1b2e430c0b9343226794ac767c6cf60
matias acuna <agroang@gmail.com>
2022-01-17 18:35:49 +0900

    added a few more extra test for round decimals to exact amount function and fixed the expected results messages

--- round-decimals-to-exact-amount.test.js
+++ round-decimals-to-exact-amount.test.js
@@ -1,19 +1,23 @@
 const round = require('./round-decimals-to-exact-amount')
 
-test('returns the number 1.005 with two decimals after the comma', () => {
+test('returns the number 1.005 with 2 decimals after the comma', () => {
   expect(round(1.005, 2)).toBe(1.01);
 });
 
-test('returns the number 1.005 with two decimals after the comma', () => {
+test('returns the number 5.105 with 1 decimal after the comma', () => {
   expect(round(5.105, 1)).toBe(5.1);
 });
 
-test('returns the number 1.005 with two decimals after the comma', () => {
+test('returns the number 4.005 with 3 decimals after the comma', () => {
   expect(round(4.005, 3)).toBe(4.005);
 });
 
-test('returns the number 1.005 with two decimals after the comma', () => {
+test('returns the number 567.163 with 2 decimals after the comma', () => {
   expect(round(567.163, 1)).toBe(567.2);
+});
+
+test('returns the number 10.98 with 1 decimals after the comma', () => {
+  expect(round(10.98, 1)).toBe(11.0);
 });
 
 test('array to be truthy', () => {
