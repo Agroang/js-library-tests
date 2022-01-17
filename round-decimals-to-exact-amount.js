051081c8e7fd66ce43ba519d2d4b689d86e0a7ce
matias acuna <agroang@gmail.com>
2022-01-17 18:33:01 +0900

    found a bug/limitation on one of the functions, will keep it like it is but with a warning regarding use

--- round-decimals-to-exact-amount.js
+++ round-decimals-to-exact-amount.js
@@ -1,4 +1,6 @@
-// function that round decimals to a certain number of decimal places
+// function that *round* decimals to a certain number of decimal places
+// **Warning**: bugging with numbers that have more than 3 commas as the input
+// is not picking the whole number, just the first 3 numbers after the comma.
 function round(n, d) {
   return Number(Math.round(n + "e" + d) + "e-" + d);
 }
--- round-decimals-to-exact-amount.test.js
+++ round-decimals-to-exact-amount.test.js
@@ -3,3 +3,19 @@ const round = require('./round-decimals-to-exact-amount')
 test('returns the number 1.005 with two decimals after the comma', () => {
   expect(round(1.005, 2)).toBe(1.01);
 });
+
+test('returns the number 1.005 with two decimals after the comma', () => {
+  expect(round(5.105, 1)).toBe(5.1);
+});
+
+test('returns the number 1.005 with two decimals after the comma', () => {
+  expect(round(4.005, 3)).toBe(4.005);
+});
+
+test('returns the number 1.005 with two decimals after the comma', () => {
+  expect(round(567.163, 1)).toBe(567.2);
+});
+
+test('array to be truthy', () => {
+  expect(round(567.163, 1)).toBeTruthy();
+});
